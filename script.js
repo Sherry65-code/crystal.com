2ac14f6d0c5542f8b1d5cc52d5df437ca8456d64
Sherry65-code <70046342+Sherry65-code@users.noreply.github.com>
2021-10-12 14:24:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,15 @@ document.body.innerHTML = `  <div class="unas" style="text-align: left;">
      <button onclick="window.open('doc.html')">View Documentation</button>
 <script src="./script.js"></script>`;
 }});
+window.addEventListener('resize' ,()=>{
+    if (innerWidth <= 642){
+document.body.innerHTML = `  <div class="unas" style="text-align: left;">
+<img alt="" class="logo"></div>
+<h1 class="a">Invento Crystal OS</h1>
+<a href="https://drive.google.com/u/0/uc?export=download&confirm=_GEs&id=1wAGrOdSJR7VFR5FDgxr1Iws8fIIMovF_" download> 
+<button class="download">Download</button></a><br><br><br>
+<h1>Bringing Technologies to Cloud. <br> We are <span> Invento </span>.We believe in <br>
+     cloud more than in local.</h1><br><br><br><br><br><br><br><br><br><img src="https://sherry65-code.github.io/imgs/img1.png" alt="" class="dis1" style="position:relative;width:90%;"><br><br><br>
+     <button onclick="window.open('doc.html')">View Documentation</button>
+<script src="./script.js"></script>`;
+}});
